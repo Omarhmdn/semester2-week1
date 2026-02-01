b133a674f41224c5a8650f020c0f8e84a70ef12f
Omarhmdn <jcsk0685@leeds.ac.uk>
2026-02-01 00:13:18 +0000

    Attempted submission hopefully final 2.1

--- worksheet/task2/question1.sql
+++ worksheet/task2/question1.sql
@@ -1,3 +1,5 @@
 -- List all students who enrolled in the year 2022.
 -- Expected Columns:
 -- StudentId, FirstName, LastName, EnrolmentYear
+
+SELECT student.StudentId, student.FirstName, student.LastName, student.EnrolmentYear FROM Student WHERE student.EnrolmentYear = 2022;
--- worksheet/task2/question2.sql
+++ worksheet/task2/question2.sql
@@ -1,3 +1,6 @@
 -- Find the 5 youngest students in the database.
 -- Expected Columns:
 -- StudentId, FirstName, LastName, DateOfBirth
+
+
+SELECT student.StudentId, student.FirstName, student.LastName, student.DateOfBirth FROM Student ORDER BY student.DateOfBirth DESC limit 5;
